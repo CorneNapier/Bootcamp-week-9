4b0ad4900a74c69f28055a6aab0dbaf358f39845
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-02-19 10:04:07 +0000

    Create day1.py
    Added examples of print() in use

---
+++ Python Week1/Day1/day1.py
@@ -0,0 +1,16 @@
+"Single line comment!"
+
+"""
+Mult line comment!
+"""
+# single line comment using hashtag
+
+# prints the specified message to the screen, or other standard output device. reminds me of console.log() in js
+print(2 * 2)
+print(2 - 2)
+print(2 + 2)
+print(15 / 5)
+
+# \n = newline
+print('Refactored code', '\n', ' ')
+print('Corné Napier', '\n', 'London', '\n', 'Gaming, Coding and Exercising')
