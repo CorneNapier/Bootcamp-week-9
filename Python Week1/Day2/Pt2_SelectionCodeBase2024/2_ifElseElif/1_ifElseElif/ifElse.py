66ef92cd12f8904d6eb0749fdf88fdaca8ba4945
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-02-20 11:44:37 +0000

    Update ifElse.py
    Added examples of if else statements in python and completed tasks

--- Python Week1/Day2/Pt2_SelectionCodeBase2024/2_ifElseElif/1_ifElseElif/ifElse.py
+++ Python Week1/Day2/Pt2_SelectionCodeBase2024/2_ifElseElif/1_ifElseElif/ifElse.py
@@ -8,37 +8,34 @@ if score < userNum:  # check the condition that score(25) is less than userNum(?
     # do something/execute the code block
     print(f"Good score {userNum}") # f-string
 
-
-
-
-
-
-
-
 # Selection is used to control the flow of the program
 
-score = 26
-userNum = 25  # int(input("Enter a number: "))
-
-"""if condition is met:
-    code block"""
-
-""" Else:
-    code block"""
-
-
 "Predict, then Run, and then Investigate"
-# check the condition that score is greater than userNum(25)
+# check the condition that pStudy value is same as the value held in curProgram
+# do something/execute the line of code if the condition is checked above true/met - welcome
+# The block(else) of code will be executed if the condition in the if block is not true/not met
 
-# do something/execute the line of code below, if the condition checked above is true/met
+pStudy = input("Enter your program of study: ").title()
+curProgram = "Bootcamp"
 
-# do something/execute the line of code below if the condition checked above is not true/ not met 
-
-
-
+if pStudy == curProgram:
+    print(f'Well done, you are right! Answer: {pStudy}')
+else:
+    print(f'Try again! Answer: {pStudy}')
 
 "To Do: Q&A"
-#What differs from writing if statement in JS and python ?
+#!What differs from writing if statement in JS and python ?
+#JS uses () for condition and {} for code statement whereas in python condition is written without () and code stament follows : using indentation
+
+score = 26
+userNum = int(input("Enter a number: "))
+
+if score == userNum:
+    print(f'Hit the nail on the head, score {userNum}')
+elif score < userNum:
+    print(f'Great score, score {userNum}')
+else:
+    print('Try again')
 
 #ToDo: Task 1:
 "Modify the code above to use any one of the comparison operators below"
